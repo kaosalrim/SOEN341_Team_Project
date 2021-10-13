29b72182983fd38f31ded94b548871a6a081a4a8
Mubashir <mubashirqbl@gmail.com>
2021-10-12 21:18:52 -0400

    User Controller Added : Added Sign IN controller Added Sign Out Controller Added Logout Controller

--- WEB_Q_A/API/Entities/AppUser.cs
+++ WEB_Q_A/API/Entities/AppUser.cs
@@ -7,7 +7,12 @@ namespace API.Entities
 {
     public class AppUser
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string DateJoined { get; set; }
+        public string  UserName { get; set; }
+        public string Password { get; set; }
+
     }
 }
--- WEB_Q_A/API/Startup.cs
+++ WEB_Q_A/API/Startup.cs
@@ -1,24 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.Handler;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-
 namespace API
 {
     public class Startup
     {
         private readonly IConfiguration _config;
+
+        public bool ValidateIssuer { get; private set; }
+        public bool ValidateAudience { get; private set; }
 
         public Startup(IConfiguration config)
         {
@@ -34,8 +44,43 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddEntityFrameworkStores<DataContext>();
+            var serve = services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
+            }
+           );
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => options.LoginPath = "/api/users/Login");
+            IdentityBuilder builder = services.AddIdentityCore<ApplicationUser>();
+
+            builder = new IdentityBuilder(builder.UserType, builder.Services);
+
+            builder.AddEntityFrameworkStores<DataContext>();
+
+            builder.AddSignInManager<SignInManager<ApplicationUser>>();
+            /*
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               {
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuerSigningKey = true,
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                             .GetBytes(_config.GetSection("AppSettings:key").Value)),
+                       ValidateIssuer = false,
+                       ValidateAudience = false
+                   };
+
+               });
+            */
             services.AddControllers();
-            services.AddCors();
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -50,6 +95,7 @@ namespace API
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+               
             }
 
             app.UseHttpsRedirection();
@@ -58,6 +104,7 @@ namespace API
 
             app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
