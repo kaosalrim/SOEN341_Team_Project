8fc4c5219466e850a228a67a7293505d8a00abca
HAOlleik <hussein.olleik5@gmail.com>
2021-10-14 11:32:07 -0400

    Created First Edition of QA Models
    Added the main variables of  the class

---
+++ WEB_Q_A/API/Entities/Answer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace API.Entities
+{
+    public Answer
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public String AnswerDetails { get; set; }
+        
+        public DateTime DatePosted { get; set; }
+        
+        [Required]
+        public int QuestionId { get; set; }
+        
+        [Required]
+        public int UserId { get; set; }
+
+        public virtual Question Question { get; set; }
+       
+        public virtual AppUser User  { get; set; }
+    }
+}
---
+++ WEB_Q_A/API/Entities/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+    
+        public String QuestionDetails { get; set; }
+
+        public DateTime DatePosted { get; set; }
+
+        [Required]
+        [StringLength(20), ErrorMessage = "No User Id length is more than 20 characters!"]
+        public int UserId { get; set; }
+       
+        public virtual AppUser User { get; set; }
+
+    }
+}
