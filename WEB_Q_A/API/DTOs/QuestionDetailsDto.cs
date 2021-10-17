5cbc4a617dca43220d59d6ddc45b77e700de98f1
Josephj9720 <Josephj9720@gmail.com>
2021-10-17 01:58:01 -0400

    Created Questions Controller and DTOs

---
+++ WEB_Q_A/API/DTOs/QuestionDetailsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class QuestionDetailsDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string QuestionDetails { get; set; }
+    }
+}
