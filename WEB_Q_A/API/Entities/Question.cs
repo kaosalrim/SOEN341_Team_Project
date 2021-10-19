8899b459b5b8489c433181e44e7423815b4ab74c
Gulnoor Kaur <75463419+gul2223@users.noreply.github.com>
2021-10-19 19:23:01 -0400

    Revert "Upvote downvote"

--- WEB_Q_A/API/Entities/Question.cs
+++ WEB_Q_A/API/Entities/Question.cs
@@ -10,9 +10,6 @@ namespace API.Entities
         public int Id { get; set; }
         public string QuestionTitle { get; set; }    
         public string QuestionDetails { get; set; }
-        public int Rank { get; set; } = 0;
-        public Boolean BestQuestion { get; set; } = false;
-        public int Views { get; set; } = 0;
         public DateTime DatePosted { get; set; } = DateTime.Now;
         public ICollection<Answer> Answers { get; set; }
         public AppUser AppUser { get; set; }
