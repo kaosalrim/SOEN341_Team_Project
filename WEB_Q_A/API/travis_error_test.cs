57be7a52b43a85c821acfe1e5e6f76013c9b6f9b
kaosalrim <artemegoroff84@gmail.com>
2021-11-06 15:11:57 -0400

    testing error response for travis

---
+++ WEB_Q_A/API/travis_error_test.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public static class travis_error_test
+    {
+        public static void Test(){
+            var x = 0;
+            var y = 9;
+            var z = x/y;
+        }
+    }
+}
