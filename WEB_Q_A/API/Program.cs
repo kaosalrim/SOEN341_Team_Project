253099cd1a8cd53a712fc776575faa32eb1fec69
kaosalrim <artemegoroff84@gmail.com>
2021-11-06 16:06:06 -0400

    post travis testing clean up

--- WEB_Q_A/API/Program.cs
+++ WEB_Q_A/API/Program.cs
@@ -14,7 +14,6 @@ namespace API
     {
         public static async Task Main(string[] args)
         {
-            travis_error_test.Test();
             var host = CreateHostBuilder(args).Build();
             using var scope = host.Services.CreateScope();
             var services = scope.ServiceProvider;
