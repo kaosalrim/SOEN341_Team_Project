d506942de12845ffcee2f01aadd0430cc739653e
HAOlleik <hussein.olleik5@gmail.com>
2021-11-09 20:12:50 -0500

    Removed my edits
    Removed whats added

--- WEB_Q_A/API/Controllers/QuestionsController.cs
+++ WEB_Q_A/API/Controllers/QuestionsController.cs
@@ -80,13 +80,6 @@ namespace API.Controllers
             return BadRequest("Failed to update the question");
         }
 
-        public ActionResult PostQuestions(QuestionDto questionDto)
-        {
-            for (QuestionDto questionDto: QuestionDto )
-            {
-
-            }
-        }
 
         [Authorize]
         [HttpPost]
