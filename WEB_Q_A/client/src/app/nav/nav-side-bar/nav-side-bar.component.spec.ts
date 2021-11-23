dd8ea5bffc84c8cca4b0f9f5720fa46809ac16ad
kaosalrim <artemegoroff84@gmail.com>
2021-11-22 19:05:31 -0500

    check if test works

--- WEB_Q_A/client/src/app/nav/nav-side-bar/nav-side-bar.component.spec.ts
+++ WEB_Q_A/client/src/app/nav/nav-side-bar/nav-side-bar.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { NavSideBarComponent } from './nav-side-bar.component';
 
 describe('NavSideBarComponent', () => {
@@ -8,7 +8,8 @@ describe('NavSideBarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavSideBarComponent ]
+      declarations: [ NavSideBarComponent ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
