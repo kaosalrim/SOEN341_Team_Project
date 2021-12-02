45fa948c154ec3134ea41f68a4ed4f0835b8c232
bob bob <rtuo@live.ca>
2021-12-02 03:48:32 -0500

    MemberSince test case

---
+++ WEB_Q_A/WEB_Q_A.Tests/MemberSinceTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.Controllers;
+using API.DTOs;
+using API.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq; //to create fakes for interfaces and classes as needed
+using API.Classes;
+
+namespace WEB_Q_A.Tests
+{
+    [TestClass]
+    public class MemberSinceTest
+    {
+
+        [TestMethod]
+        public Task AssignDate_DateTakesInValues_RetrurnDate()
+        {
+            // Arrange
+            var memberSince = new MemberSince();
+
+            // Act
+            var result = memberSince.Days = 10;
+
+
+            // Assert
+            Assert.IsNotNull(result);
+            return Task.CompletedTask;
+        }
+    }
+}
